26945c34b50a532e2901fda8531265ed29b58066
GehadHisham20 <73800429+GehadHisham20@users.noreply.github.com>
2022-03-02 22:49:58 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react'
+import Home from './components/Home/Home'
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Home/>
+      </>
+    )
+  }
+}
+
+
